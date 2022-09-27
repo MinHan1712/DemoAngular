329d22995919e4b9446ec5f8a4a7d76441572ecc
Han Min <nguyenminhhanhang@gmail.com>
2022-09-27 23:46:39 +0700

    demo angular

---
+++ SuperHeroNG/SuperHeroAPI/SuperHeroAPI/Migrations/20220923050028_superheroDb_UpdateSuperHero.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SuperHeroAPI.Migrations
+{
+    public partial class superheroDb_UpdateSuperHero : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "lastName",
+                table: "SuperHeroes",
+                newName: "LastName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "SuperHeroes",
+                newName: "lastName");
+        }
+    }
+}
